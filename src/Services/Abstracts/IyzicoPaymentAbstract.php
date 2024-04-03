94e5066765be2c3fd0609bb059e4e226fdc360bb
Erdal ALTIN <erdalaltin@gmail.com>
2024-04-03 11:22:24 +0300

    Update IyzicoPaymentAbstract.php

--- src/Services/Abstracts/IyzicoPaymentAbstract.php
+++ src/Services/Abstracts/IyzicoPaymentAbstract.php
@@ -4,12 +4,18 @@ namespace Botble\Iyzico\Services\Abstracts;
 
 use Botble\Payment\Services\Traits\PaymentErrorTrait;
 use Botble\Support\Services\ProduceServiceInterface;
+use Botble\Payment\Repositories\Interfaces\PaymentInterface;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 use Botble\Iyzico\Http\Controllers\IyzicoConfig;
+use Iyzipay\Request\CreateRefundRequest;
+use Iyzipay\Request\ReportingPaymentDetailRequest;
+use Iyzipay\Model\ReportingPaymentDetail;
+use Iyzipay\Model\Refund;
+use Iyzipay\Model\Locale;
 use Iyzipay\Options;
 
 abstract class IyzicoPaymentAbstract implements ProduceServiceInterface
@@ -17,35 +23,33 @@ abstract class IyzicoPaymentAbstract implements ProduceServiceInterface
     use PaymentErrorTrait;
 
     protected string $paymentCurrency;
-
-    /**
+	
+	 /**
      * @var object
      */
-    protected $client;
+    protected $api;
 
     protected bool $supportRefundOnline;
-
-    protected float $totalAmount;
 
     public function __construct()
     {
         $this->paymentCurrency = config('plugins.payment.payment.currency');
 
-        $this->totalAmount = 0;
-
-        $this->setClient();
+        $this->setApi();
 
         $this->supportRefundOnline = true;
+
     }
 
     public function getSupportRefundOnline(): bool
     {
         return $this->supportRefundOnline;
     }
-
-    public function setClient(): self
-    {		
-		$this->client = (new IyzicoConfig)->options();
+	
+	public function setApi(): self
+    {
+		# create request class
+        $this->api = new CreateRefundRequest();
 
         return $this;
     }
@@ -53,27 +57,19 @@ abstract class IyzicoPaymentAbstract implements ProduceServiceInterface
     /**
      * @return object
      */
-    public function getClient()
+    public function getApi()
     {
-        return $this->client;
+        return $this->api;
     }
 	
-    public function setCurrency(string $currency): self
-    {
-        $this->paymentCurrency = $currency;
-
-        return $this;
-    }
-
-    public function getCurrency(): string|null
-    {
-        return $this->paymentCurrency;
-    }
-
-    public function getPaymentDetails(string $paymentId)
+	public function getPaymentDetails(string $conversationId)
     {
         try {
-            $response  = $this->client->payments->get($paymentId); // Returns a particular payment
+
+			$this->api = new ReportingPaymentDetailRequest();
+			$this->api->setPaymentConversationId($conversationId);
+			$response = ReportingPaymentDetail::create($this->api, (new IyzicoConfig)->options());
+
         } catch (Exception $exception) {
             $this->setErrorMessageAndLogging($exception, 1);
 
@@ -82,41 +78,49 @@ abstract class IyzicoPaymentAbstract implements ProduceServiceInterface
 
         return $response;
     }
-
+	
     /**
      * This function can be used to preform refund on the capture.
      */
     public function refundOrder($paymentId, $amount, array $options = [])
     {
         try {
-            $payment = $this->client->payments->get($paymentId);
 
-            if ($payment->canBeRefunded() &&
-                $payment->amountRemaining->currency == $this->paymentCurrency &&
-                (float) $payment->amountRemaining->value >= (float) $amount) {
-                /*
-                 * https://docs.iyzico.com/reference/v2/refunds-api/create-refund
-                 */
-                $description = Arr::get($options, 'refund_note') ?: get_order_code(Arr::get($options, 'order_id'));
-                $refund = $payment->refund([
-                    'amount' => [
-                        'currency' => $this->paymentCurrency,
-                        'value' => number_format((float) $amount, 2, '.', ''), // You must send the correct number of decimals, thus we enforce the use of strings
-                    ],
-                    'description' => Str::limit($description, 140),
-                    'metadata' => $options,
-                ]);
+			$this->api->setLocale(\Request::getLocale());
+			
+			$payment = app(PaymentInterface::class)->getFirstBy([
+				'charge_id' => $paymentId,
+				['order_id', 'IN', array_map('intval', explode(',', Arr::get($options, 'order_id', 0)))],
+			]);
+			$this->api->setConversationId(Arr::get($payment->metadata, 'conversation_id', ''));
+			
+			$this->api->setPaymentTransactionId(Arr::get($payment->metadata, 'paymentTransaction_id', ''));
+			$this->api->setPrice(number_format((float) $amount, 2, '.', ''));
+			$currency = $this->paymentCurrency;
+            if($currency == "TRY") $currency = "TL";
+			$this->api->setCurrency(constant('Iyzipay\Model\Currency::' . $currency));
+			
+			$this->api->setIp(\Request::ip());
+			$this->api->setReason(\Iyzipay\Model\RefundReason::OTHER);			
+			$description = 'Order information: ' . get_order_code(Arr::get($options, 'order_id', ''));
+			$description .= ', Detail: ' . Arr::get($options, 'refund_note', '');
+			$this->api->setDescription(Str::limit($description, 140));
+
+			# make request
+			$refund = Refund::create($this->api, (new IyzicoConfig)->options());
+
+            if ( $refund->getStatus() == \Iyzipay\Model\Status::SUCCESS && $refund->getCurrency() == $this->paymentCurrency && (float) $refund->getPrice() >= (float) $amount) {                
 
                 return [
                     'error' => false,
-                    'message' => "{$refund->amount->currency} {$refund->amount->value} of payment {$paymentId} refunded.",
+                    'message' => "{$refund->getCurrency()} {$refund->getPrice()} of payment {$paymentId} refunded.",
                     'data' => (array) $refund,
                 ];
             }
 
             return [
                 'error' => true,
-                'message' => "Payment {$paymentId} can not be refunded.",
+		        'message' => "Payment {$paymentId} can not be refunded. Code: {$refund->geterrorCode()}, Message: {$refund->geterrorMessage()}",
             ];
         } catch (Exception $exception) {
             $this->setErrorMessageAndLogging($exception, 1);
